aef8efe20f1ffccbd216453955f4634807f81480
shudh25 <pandey252001@gmail.com>
2023-08-04 15:55:50 +0530

    ALL Done

--- assets/script.js
+++ assets/script.js
@@ -5,84 +5,84 @@ const email = document.getElementById('email')
 const errorElement = document.getElementById('error')
 const submitRes = document.getElementById('submitRes')
 
+
 formE1.addEventListener('submit', (e) => {
 
-
-    function formValidation() {
-
-        let error_messages = []
-
-        // PHONE VALIDATIONS
-
-        var numbers = /^[0-9]+$/;
-
-        if (phone.value.match(numbers) == null) {
-            error_messages.push("Enter Number only in Phone")
-            console.log("hello")
-        }
-
-        // If the Entered Phone Is less than 10 Digit
-        let phone_length = phone.value.toString().length
-        if (phone_length < 10) {
-            error_messages.push("PLease Enter valid Phone Number")
-        }
-
-
-        //FILE VALIDATIONS
-
-        var InputFile = resume;
-        var filePath = InputFile.value;
-
-        // Allowing file type
-        var allowedExtensions = /(\.pdf|\.png)$/i;
-
-        if (!allowedExtensions.exec(filePath)) {
-            error_messages.push('Invalid file type');
-            InputFile.value = '';
-        }
-
-
-        //EMAIL VALIDATIONS
-
-        var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@(xenonstack.in|xenonstack.com)$/;
-        var receivedEmail = email.value.trim();
-
-        //Checking Custom Domain Vaidation
-        if (!(receivedEmail.toLowerCase().match(validRegex))) {
-            error_messages.push("Please enter valid email address.")
-        }
-
-
-        // Printing All error_messages
-        if (error_messages.length > 0) {
-            e.preventDefault()
-            errorElement.innerText = error_messages.join(', ')
-            errorElement.style.height = "10px";
-            console.log(error_messages.length)
-        } else {
-            return true
-        }
+    e.preventDefault();
+    if (formValidation(e)) {
+        errorElement.style.display = "none";
+        send(e);
+        submitRes.innerText = "Data Submitted Sucessfully";
+        formE1.reset();
     }
-
-
-    if (formValidation()) {
-        console.log(error_messages.length)
-        console.log(error_messages.length <= 0)
-
-        e.preventDefault();
-
-        const data = new FormData(formE1);
-        data.append("Resume", resume.files[0]);
-
-        fetch('/sendData', {
-            method: 'POST',
-            body: data
-        }).then(res => res.json())
-            .then(data => console.log(data))
-            .then(error => console.log(error))
-    }
-
-
 })
 
+function formValidation(e) {
 
+    let error_messages = []
+
+    // PHONE VALIDATIONS
+
+    var numbers = /^[0-9]+$/;
+
+    if (phone.value.match(numbers) == null) {
+        error_messages.push("Enter Number only in Phone")
+        console.log("hello")
+    }
+
+    // If the Entered Phone Is less than 10 Digit
+    let phone_length = phone.value.toString().length
+    if (phone_length < 10) {
+        error_messages.push("PLease Enter valid Phone Number")
+    }
+
+
+    //FILE VALIDATIONS
+
+    var InputFile = resume;
+    var filePath = InputFile.value;
+
+    // Allowing file type
+    var allowedExtensions = /(\.pdf|\.png)$/i;
+
+    if (!allowedExtensions.exec(filePath)) {
+        error_messages.push('Invalid file type');
+        InputFile.value = '';
+    }
+
+
+    //EMAIL VALIDATIONS
+
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@(xenonstack.in|xenonstack.com)$/;
+    var receivedEmail = email.value.trim();
+
+    //Checking Custom Domain Vaidation
+    if (!(receivedEmail.toLowerCase().match(validRegex))) {
+        error_messages.push("Please enter valid email address.")
+    }
+
+
+    // Printing All error_messages
+    if (error_messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = error_messages.join(', ')
+        errorElement.style.height = "10px";
+        return false;
+    }
+    return true;
+}
+
+function send(event) {
+
+    event.preventDefault();
+
+    const data = new FormData(formE1);
+    data.append("Resume", resume.files[0]);
+
+    fetch('/sendData', {
+        method: 'POST',
+        body: data
+    }).then(res => res.json())
+        .then(data => console.log(data))
+        .then(error => console.log(error))
+}
