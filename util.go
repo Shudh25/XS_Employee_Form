97a5d9bceb1ed9bb36176b5fa8027ca48dc2d7c2
shudh25 <pandey252001@gmail.com>
2023-08-07 13:36:45 +0530

    file update

--- util.go
+++ util.go
@@ -7,14 +7,11 @@ import (
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
-	"gorm.io/gorm"
 )
 
 // Custom DATA TYPE STRUCTURE
-
 type Employee struct {
-	gorm.Model
-	Id       int       `json:"id" gorm:"primaryKey"`
+	Id       int       `json:"id"`
 	Name     string    `json:"name"`
 	Gender   string    `json:"gender"`
 	FromDate time.Time `json:"from"`
@@ -53,8 +50,6 @@ func db_connection() (db *sql.DB) {
 	db, err := sql.Open("postgres", psqlconn)
 	CheckError(err)
 
-	// close database
-	// defer db.Close()
 	return db
 }
 
